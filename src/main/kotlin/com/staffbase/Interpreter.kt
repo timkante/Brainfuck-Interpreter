faea78d16b4075c385a426d2182ea095ca64cc8c
Tim Kante <tim.kante@staffbase.com>
2020-02-16 05:10:13 +0100

    :bug: fixed logical error in the loop-logic

--- src/main/kotlin/com/staffbase/Interpreter.kt
+++ src/main/kotlin/com/staffbase/Interpreter.kt
@@ -1,53 +1,49 @@
 package com.staffbase
 
+import java.io.IOException
+
 class Interpreter {
 
     companion object{
-        const val MAX_MEMSIZE = 1024
+        const val MAX_MEMSIZE = 65536
     }
 
     data class State (
         val currentPointer: Int = 0,
-        val memory: ByteArray = ByteArray(MAX_MEMSIZE),
+        val memory: IntArray = IntArray(MAX_MEMSIZE),
         val output: String = ""
     ) {
-        val currentChar: Char
-            get() = memory[currentPointer].toChar()
+        val currentValue: Int get() = memory[currentPointer]
 
-        val incremented: State
-            get() = copy(memory = memory.also {
-                if (it[currentPointer] == Byte.MAX_VALUE) throw ArithmeticException("Value overflow")
-                else ++it[currentPointer]
-            })
-
-        val decremented: State
-            get() = copy(memory = memory.also {
-                if (it[currentPointer] == Byte.MIN_VALUE) throw ArithmeticException("Value underflow")
-                else --it[currentPointer]
-            })
-
-        val rightShifted: State
-            get()= when(currentPointer) {
-                MAX_MEMSIZE -> throw StackOverflowError("Exceeding memory upper bound")
-                else -> copy(currentPointer = currentPointer + 1)
-            }
-
-        val leftShifted: State
-            get() = when(currentPointer) {
-                0 -> throw StackOverflowError("Exceeding memory lower bound")
-                else -> copy(currentPointer = currentPointer - 1)
-            }
-
-        fun putChar(newChar: Char) = copy(memory = memory.also {
-            it[currentPointer] = newChar.toByte()
+        val incremented: State get() = copy(memory = memory.also {
+            if (it[currentPointer] == Int.MAX_VALUE) throw ArithmeticException("Value overflow")
+            else ++it[currentPointer]
         })
 
-        val outputApplied: State
-            get() = copy(output = output + currentChar)
-    }
+        val decremented: State get() = copy(memory = memory.also {
+            if (it[currentPointer] == Int.MIN_VALUE) throw ArithmeticException("Value underflow")
+            else --it[currentPointer]
+        })
 
-    private val String.firstOpeningBraceIndex: Int
-        get () = this.indexOfFirst { it == '[' }
+        val rightShifted: State get()= when(currentPointer) {
+            MAX_MEMSIZE -> throw StackOverflowError("Exceeding memory upper bound")
+            else -> copy(currentPointer = currentPointer + 1)
+        }
+
+        val leftShifted: State get() = when(currentPointer) {
+            0 -> throw StackOverflowError("Exceeding memory lower bound")
+            else -> copy(currentPointer = currentPointer - 1)
+        }
+
+        fun putChar(newChar: Char) = copy(memory = memory.also {
+            it[currentPointer] = newChar.toInt()
+        })
+
+        val outputApplied: State get() = copy(output = output + (currentValue.takeIf { when(it){
+            in 0..Char.MAX_VALUE.toInt() -> true
+            else -> false
+        }}?.toChar() ?: throw IOException()))
+    }
 
     private val String.isValidProgram: Boolean
         get () = filter{ it in "[]"}
@@ -55,23 +51,40 @@ class Interpreter {
             .eachCount()
             .let { it['['] == it[']'] }
 
-    fun interpret(program: String) = when(program.isValidProgram){
-        true -> interpret(program, State())
-        else -> throw IllegalArgumentException("The program is not valid (brackets dont match)")
-    }
-
-    private fun interpret(program: String, interpretingState: State): State =
-        program.fold(interpretingState){ prevState, instruction ->
-            when(instruction){
-                '+' -> prevState.incremented
-                '-' -> prevState.decremented
-                '>' -> prevState.rightShifted
-                '<' -> prevState.leftShifted
-                '.' -> prevState.outputApplied
-                ',' -> prevState.putChar(readLine()?.get(0) ?: Char.MIN_VALUE)
-                '[' -> return interpret(program.substring(program.firstOpeningBraceIndex + 1), prevState)
-                ']' -> if (prevState.currentChar != 0.toChar()) return interpret(program, prevState) else prevState
-                else -> prevState
+    fun interpret(program: String): State {
+        if (!program.isValidProgram) throw IllegalArgumentException("The program is not valid (brackets dont match)")
+        var state = State()
+        var balance: Int = 0
+        var index: Int = 0
+        while (index < program.length){
+            when(program[index]){
+                '+' -> state = state.incremented
+                '-' -> state = state.decremented
+                '>' -> state = state.rightShifted
+                '<' -> state = state.leftShifted
+                '.' -> state = state.outputApplied
+                ',' -> state = state.putChar(readLine()?.get(0) ?: Char.MIN_VALUE)
+                '[' -> if (state.currentValue == 0){
+                    ++index
+                    while (balance > 0 || program[index] != ']'){
+                        if (program[index] == '[') ++balance
+                        else if (program[index] == ']') --balance
+                        ++index
+                    }
+                }
+                ']' -> if (state.currentValue != 0){
+                    --index
+                    while (balance > 0 || program[index] != '['){
+                        if (program[index] == ']') ++balance
+                        else if (program[index] == '[') --balance
+                        --index
+                    }
+                    --index
+                }
+                else -> {}
             }
+            ++index
+        }
+        return state
     }
 }
--- src/test/kotlin/com/staffbase/InterpreterTest.kt
+++ src/test/kotlin/com/staffbase/InterpreterTest.kt
@@ -15,27 +15,253 @@ class InterpreterTest{
         const val HELLO_WORLD = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+." +
                 "+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.+++."
 
-        const val CELL_WIDTH = "Calculate the value 256 and test if it's zero\n" +
-                "If the interpreter errors on overflow this is where it'll happen\n" +
-                "++++++++[>++++++++<-]>[<++++>-]\n" +
-                "+<[>-<\n" +
-                "    Not zero so multiply by 256 again to get 65536\n" +
-                "    [>++++<-]>[<++++++++>-]<[>++++++++<-]\n" +
-                "    +>[>\n" +
-                "        # Print \"32\"\n" +
-                "        ++++++++++[>+++++<-]>+.-.[-]<\n" +
-                "    <[-]<->] <[>>\n" +
-                "        # Print \"16\"\n" +
-                "        +++++++[>+++++++<-]>.+++++.[-]<\n" +
-                "<<-]] >[>\n" +
-                "    # Print \"8\"\n" +
-                "    ++++++++[>+++++++<-]>.[-]<\n" +
-                "<-]<\n" +
-                "# Print \" bit cells\\n\"\n" +
-                "+++++++++++[>+++>+++++++++>+++++++++>+<<<<-]>-.>-.+++++++.+++++++++++.<.\n" +
-                ">>.++.+++++++..<-.>>-\n" +
-                "Clean up used cells.\n" +
-                "[[-]<]"
+        const val BOTTLES = "##########################\n" +
+                "###\n" +
+                "### Severely updated version!\n" +
+                "### (now says \"1 bottle\" and\n" +
+                "### contains no extra \"0\" verse)\n" +
+                "###\n" +
+                "##########################\n" +
+                "### 99 Bottles of Beer ###\n" +
+                "### coded in Brainfuck ###\n" +
+                "### with explanations  ###\n" +
+                "##########################\n" +
+                "#\n" +
+                "# This Bottles of Beer program\n" +
+                "# was written by Andrew Paczkowski\n" +
+                "# Coder Alias: thepacz\n" +
+                "# three_halves_plus_one@yahoo.com\n" +
+                "#####\n" +
+                "\n" +
+                ">                            0 in the zeroth cell\n" +
+                "+++++++>++++++++++[<+++++>-] 57 in the first cell or \"9\"\n" +
+                "+++++++>++++++++++[<+++++>-] 57 in second cell or \"9\"\n" +
+                "++++++++++                   10 in third cell\n" +
+                ">+++++++++                    9 in fourth cell\n" +
+                "\n" +
+                "##########################################\n" +
+                "### create ASCII chars in higher cells ###\n" +
+                "##########################################\n" +
+                "\n" +
+                ">>++++++++[<++++>-]               \" \"\n" +
+                ">++++++++++++++[<+++++++>-]        b\n" +
+                "+>+++++++++++[<++++++++++>-]       o\n" +
+                "++>+++++++++++++++++++[<++++++>-]  t\n" +
+                "++>+++++++++++++++++++[<++++++>-]  t\n" +
+                ">++++++++++++[<+++++++++>-]        l\n" +
+                "+>++++++++++[<++++++++++>-]        e\n" +
+                "+>+++++++++++++++++++[<++++++>-]   s\n" +
+                ">++++++++[<++++>-]                \" \"\n" +
+                "+>+++++++++++[<++++++++++>-]       o\n" +
+                "++>++++++++++[<++++++++++>-]       f\n" +
+                ">++++++++[<++++>-]                \" \"\n" +
+                ">++++++++++++++[<+++++++>-]        b\n" +
+                "+>++++++++++[<++++++++++>-]        e\n" +
+                "+>++++++++++[<++++++++++>-]        e\n" +
+                ">+++++++++++++++++++[<++++++>-]    r\n" +
+                ">++++++++[<++++>-]                \" \"\n" +
+                "+>+++++++++++[<++++++++++>-]       o\n" +
+                ">+++++++++++[<++++++++++>-]        n\n" +
+                ">++++++++[<++++>-]                \" \"\n" +
+                "++>+++++++++++++++++++[<++++++>-]  t\n" +
+                "++++>++++++++++[<++++++++++>-]     h\n" +
+                "+>++++++++++[<++++++++++>-]        e\n" +
+                ">++++++++[<++++>-]                \" \"\n" +
+                "++>+++++++++++++[<+++++++++>-]     w\n" +
+                "+>++++++++++++[<++++++++>-]        a\n" +
+                ">++++++++++++[<+++++++++>-]        l\n" +
+                ">++++++++++++[<+++++++++>-]        l\n" +
+                ">+++++[<++>-]                      LF\n" +
+                "++>+++++++++++++++++++[<++++++>-]  t\n" +
+                "+>++++++++++++[<++++++++>-]        a\n" +
+                "+++>+++++++++++++[<++++++++>-]     k\n" +
+                "+>++++++++++[<++++++++++>-]        e\n" +
+                ">++++++++[<++++>-]                \" \"\n" +
+                "+>+++++++++++[<++++++++++>-]       o\n" +
+                ">+++++++++++[<++++++++++>-]        n\n" +
+                "+>++++++++++[<++++++++++>-]        e\n" +
+                ">++++++++[<++++>-]                \" \"\n" +
+                ">++++++++++[<++++++++++>-]         d\n" +
+                "+>+++++++++++[<++++++++++>-]       o\n" +
+                "++>+++++++++++++[<+++++++++>-]     w\n" +
+                ">+++++++++++[<++++++++++>-]        n\n" +
+                ">++++++++[<++++>-]                \" \"\n" +
+                "+>++++++++++++[<++++++++>-]        a\n" +
+                ">+++++++++++[<++++++++++>-]        n\n" +
+                ">++++++++++[<++++++++++>-]         d\n" +
+                ">++++++++[<++++>-]                \" \"\n" +
+                "++>+++++++++++[<++++++++++>-]      p\n" +
+                "+>++++++++++++[<++++++++>-]        a\n" +
+                "+>+++++++++++++++++++[<++++++>-]   s\n" +
+                "+>+++++++++++++++++++[<++++++>-]   s\n" +
+                ">++++++++[<++++>-]                \" \"\n" +
+                "+>+++++++++++++[<++++++++>-]       i\n" +
+                "++>+++++++++++++++++++[<++++++>-]  t\n" +
+                ">++++++++[<++++>-]                \" \"\n" +
+                "+>++++++++++++[<++++++++>-]        a\n" +
+                ">+++++++++++++++++++[<++++++>-]    r\n" +
+                "+>+++++++++++[<++++++++++>-]       o\n" +
+                ">+++++++++++++[<+++++++++>-]       u\n" +
+                ">+++++++++++[<++++++++++>-]        n\n" +
+                ">++++++++++[<++++++++++>-]         d\n" +
+                ">+++++[<++>-]                      LF\n" +
+                "+++++++++++++                      CR\n" +
+                "\n" +
+                "[<]>>>>      go back to fourth cell\n" +
+                "\n" +
+                "#################################\n" +
+                "### initiate the display loop ###\n" +
+                "#################################\n" +
+                "\n" +
+                "[            loop\n" +
+                " <           back to cell 3\n" +
+                " [            loop\n" +
+                "  [>]<<       go to last cell and back to LF\n" +
+                "  ..          output 2 newlines\n" +
+                "  [<]>        go to first cell\n" +
+                "\n" +
+                " ###################################\n" +
+                " #### begin display of characters###\n" +
+                " ###################################\n" +
+                " #\n" +
+                " #.>.>>>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>\n" +
+                " #X X     b o t t l e s   o f   b e e r  \n" +
+                " #.>.>.>.>.>.>.>.>.>.>.>.\n" +
+                " #o n   t h e   w a l l N\n" +
+                " #[<]>    go to first cell\n" +
+                " #.>.>>>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>>>>>>>>>>>>>.>\n" +
+                " #X X     b o t t l e s   o f   b e e r             N\n" +
+                " #.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>\n" +
+                " #t a k e   o n e   d o w n   a n d   p a s s   \n" +
+                " #.>.>.>.>.>.>.>.>.>.\n" +
+                " #i t   a r o u n d N\n" +
+                " #####\n" +
+                "\n" +
+                "  [<]>>      go to cell 2\n" +
+                "  -          subtract 1 from cell 2\n" +
+                "  <          go to cell 1\n" +
+                "\n" +
+                " ########################\n" +
+                " ### display last line ##\n" +
+                " ########################\n" +
+                " #\n" +
+                " #.>.>>>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>\n" +
+                " #X X     b o t t l e s   o f   b e e r  \n" +
+                " #.>.>.>.>.>.>.>.>.>.>.\n" +
+                " #o n   t h e   w a l l\n" +
+                " #####\n" +
+                "\n" +
+                "  [<]>>>-      go to cell 3/subtract 1\n" +
+                " ]            end loop when cell 3 is 0\n" +
+                " ++++++++++   add 10 to cell 3\n" +
+                " <++++++++++  back to cell 2/add 10\n" +
+                " <-           back to cell 1/subtract 1\n" +
+                " [>]<.        go to last line/carriage return\n" +
+                " [<]>         go to first line\n" +
+                "\n" +
+                "########################\n" +
+                "### correct last line ##\n" +
+                "########################\n" +
+                "#\n" +
+                "#.>.>>>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>\n" +
+                "#X X     b o t t l e s   o f   b e e r  \n" +
+                "#.>.>.>.>.>.>.>.>.>.>.\n" +
+                "#o n   t h e   w a l l\n" +
+                "#####\n" +
+                "\n" +
+                " [<]>>>>-    go to cell 4/subtract 1\n" +
+                "]           end loop when cell 4 is 0\n" +
+                "\n" +
+                "##############################################################\n" +
+                "### By this point verses 99\\10 are displayed but to work   ###\n" +
+                "### with the lower numbered verses in a more readable way  ###\n" +
+                "### we initiate a new loop for verses 9{CODE} that will not    ###\n" +
+                "### use the fourth cell at all                             ###\n" +
+                "##############################################################\n" +
+                "\n" +
+                "+           add 1 to cell four (to keep it non\\zero)\n" +
+                "<--         back to cell 3/subtract 2\n" +
+                "\n" +
+                "[            loop\n" +
+                " [>]<<       go to last cell and back to LF\n" +
+                " ..          output 2 newlines\n" +
+                " [<]>        go to first cell\n" +
+                "\n" +
+                " ###################################\n" +
+                " #### begin display of characters###\n" +
+                " ###################################\n" +
+                " #\n" +
+                " #>.>>>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>\n" +
+                " # X     b o t t l e s   o f   b e e r  \n" +
+                " #.>.>.>.>.>.>.>.>.>.>.>.\n" +
+                " #o n   t h e   w a l l N\n" +
+                " #[<]>    go to first cell\n" +
+                " #>.>>>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>>>>>>>>>>>>>.>\n" +
+                " # X     b o t t l e s   o f   b e e r             N\n" +
+                " #.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>\n" +
+                " #t a k e   o n e   d o w n   a n d   p a s s   \n" +
+                " #.>.>.>.>.>.>.>.>.>.\n" +
+                " #i t   a r o u n d N\n" +
+                " #####\n" +
+                "\n" +
+                " [<]>>       go to cell 2\n" +
+                " -           subtract 1 from cell 2\n" +
+                "\n" +
+                " ########################\n" +
+                " ### display last line ##\n" +
+                " ########################\n" +
+                " #\n" +
+                " #.>>>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>\n" +
+                " #X     b o t t l e s   o f   b e e r  \n" +
+                " #.>.>.>.>.>.>.>.>.>.>.\n" +
+                " #o n   t h e   w a l l\n" +
+                " #####\n" +
+                "\n" +
+                " [<]>>>-     go to cell 3/subtract 1\n" +
+                "]            end loop when cell 3 is 0\n" +
+                "+            add 1 to cell 3 to keep it non\\zero\n" +
+                "\n" +
+                "[>]<.        go to last line/carriage return\n" +
+                "[<]>         go to first line\n" +
+                "\n" +
+                "########################\n" +
+                "### correct last line ##\n" +
+                "########################\n" +
+                "#\n" +
+                "#>.>>>.>.>.>.>.>.>.>>.>.>.>.>.>.>.>.>.>\n" +
+                "# X     b o t t l e    o f   b e e r  \n" +
+                "#.>.>.>.>.>.>.>.>.>.>.<<<<.\n" +
+                "#o n   t h e   w a l l\n" +
+                "#####\n" +
+                "\n" +
+                "[>]<<       go to last cell and back to LF\n" +
+                "..          output 2 newlines\n" +
+                "[<]>        go to first line\n" +
+                "\n" +
+                "#########################\n" +
+                "### the final verse    ##\n" +
+                "#########################\n" +
+                "#\n" +
+                "#>.>>>.>.>.>.>.>.>.>>.>.>.>.>.>.>.>.>.>\n" +
+                "# X     b o t t l e    o f   b e e r  \n" +
+                "#.>.>.>.>.>.>.>.>.>.>.>.\n" +
+                "#o n   t h e   w a l l N\n" +
+                "#[<]>        go to first cell\n" +
+                "#>.>>>.>.>.>.>.>.>.>>.>.>.>.>.>.>.>.>>>>>>>>>>>>>.>\n" +
+                "# X     b o t t l e    o f   b e e r             N\n" +
+                "#.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>\n" +
+                "#t a k e   o n e   d o w n   a n d   p a s s   \n" +
+                "#.>.>.>.>.>.>.>.>.>.\n" +
+                "#i t   a r o u n d N\n" +
+                "#[>]<        go to last line\n" +
+                "#<<<.<<.<<<.\n" +
+                "#   n  o    \n" +
+                "#[<]>>>>     go to fourth cell\n" +
+                "#>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>.>\n" +
+                "#   b o t t l e s   o f   b e e r  \n" +
+                "#.>.>.>.>.>.>.>.>.>.>.>.\n" +
+                "#o n   t h e   w a l l N\n" +
+                "#####fin##"
 
         @JvmStatic
         fun getIncrements() = (0 until 100).map {index -> Arguments.of("+".repeat(index), index)}.stream()
@@ -53,19 +279,19 @@ class InterpreterTest{
     @MethodSource("getIncrements")
     fun `can do increment`(program: String, increments: Int){
         val finishingState = interpreterInstance.interpret(program)
-        assertEquals(increments, finishingState.currentChar.toInt())
+        assertEquals(increments, finishingState.currentValue)
     }
 
     @Test
     fun `can do decrement`(){
         val finishingState = interpreterInstance.interpret("+++---")
-        assertEquals(0, finishingState.currentChar.toInt())
+        assertEquals(0, finishingState.currentValue)
     }
 
     @Test
     fun `can do increment and decrement`(){
         val finishingState = interpreterInstance.interpret("+++++++--")
-        assertEquals(5, finishingState.currentChar.toInt())
+        assertEquals(5, finishingState.currentValue)
     }
 
     @ParameterizedTest
@@ -85,19 +311,19 @@ class InterpreterTest{
     fun `can do shifts, increment and decrement`(){
         val finishingState = interpreterInstance.interpret(">++-+->><<")
         assertEquals(1, finishingState.currentPointer)
-        assertEquals(1, finishingState.currentChar.toInt())
+        assertEquals(1, finishingState.currentValue)
     }
 
     @Test
     fun `can parse empty loop`(){
         val finishingState = interpreterInstance.interpret("[]")
-        assertEquals(0, finishingState.currentChar.toInt())
+        assertEquals(0, finishingState.currentValue)
     }
 
     @Test
     fun `can parse multiple empty loops`(){
         val finishingState = interpreterInstance.interpret("[][][][]")
-        assertEquals(0, finishingState.currentChar.toInt())
+        assertEquals(0, finishingState.currentValue)
     }
 
     @Test
@@ -113,13 +339,13 @@ class InterpreterTest{
     @Test
     fun `can parse nested empty loops`(){
         val finishingState = interpreterInstance.interpret("[[[]]]")
-        assertEquals(0, finishingState.currentChar.toInt())
+        assertEquals(0, finishingState.currentValue)
     }
 
     @Test
     fun `can parse multiple nested empty loops`(){
         val finishingState = interpreterInstance.interpret("[[][[][]]]")
-        assertEquals(0, finishingState.currentChar.toInt())
+        assertEquals(0, finishingState.currentValue)
     }
 
    @Test
@@ -129,9 +355,8 @@ class InterpreterTest{
    }
 
     @Test
-    fun `can test cell width`(){
-        assertThrows(ArithmeticException::class.java) {
-            interpreterInstance.interpret(CELL_WIDTH)
-        }
+    fun `can parse bottle program`(){
+        val result = interpreterInstance.interpret(BOTTLES)
+        assertNotEquals("", result.output)
     }
 }
